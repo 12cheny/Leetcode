e9635c888b69b909ff20d08fafbe57f31ed6b2e6
12cheny <2733743979@qq.com>
2018-05-10 11:27:18 +0800

    Create day9.py

---
+++ day9.py
@@ -0,0 +1,10 @@
+#Write a function to delete a node (except the tail) in a singly linked list, given only access to that node. 
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val=node.next.val
+        node.next=node.next.next

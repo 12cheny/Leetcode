8c8f9cd022d6eb2c8ced431ccdb4b4e8672779f5
12cheny <2733743979@qq.com>
2018-05-14 19:07:18 +0800

    Create day_015.py

---
+++ day_015.py
@@ -0,0 +1,12 @@
+#Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.append(target)
+        nums.sort()
+        return nums.index(target)

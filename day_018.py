96afab8f31e8860bdadf8b82d15f1f882150175d
12cheny <2733743979@qq.com>
2018-05-14 22:02:36 +0800

    Create day_018.py

---
+++ day_018.py
@@ -0,0 +1,9 @@
+#Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return bin(n).count('1')

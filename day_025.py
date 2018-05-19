b8156292490d3531a3da3ec7634b3c9a917c9ba3
12cheny <2733743979@qq.com>
2018-05-19 18:01:35 +0800

    Create day_025.py

---
+++ day_025.py
@@ -0,0 +1,17 @@
+#Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if matrix==[[]] or matrix==[]:
+            return False
+        for i in range(len(matrix)):
+            if target in matrix[i]:
+                return True
+            elif target<matrix[i][len(matrix[i])-1]:
+                return False
+        return False

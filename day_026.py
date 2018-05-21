6abf53471f5a4e9b4b1e9f091080e69094b49257
12cheny <2733743979@qq.com>
2018-05-21 14:51:34 +0800

    Create day_026.py

---
+++ day_026.py
@@ -0,0 +1,10 @@
+#add two binary
+
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        return '{0:b}'.format(int(a,2)+int(b,2))
